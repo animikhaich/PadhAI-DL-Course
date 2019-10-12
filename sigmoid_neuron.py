7c8aedea1a31dd5cdc6c511dbd85b61979b2d1b4
Animikh Aich <animikh@wobot.ai>
2019-10-13 00:43:15 +0530

    Added TQDM progress bar and loss plot

--- sigmoid_neuron.py
+++ sigmoid_neuron.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.metrics import mean_squared_error
+from tqdm import tqdm, tqdm_notebook
+from matplotlib import pyplot as plt
 
 class SigmoidNeuron:
     def __init__(self):
@@ -19,12 +22,15 @@ class SigmoidNeuron:
         y_pred = self.sigmoid(self.perceptron(x))
         return (y_pred - y) * y_pred * (1 - y_pred)
     
-    def fit(self, X, Y, epochs=10, learning_rate=0.01, initialize=True):
+    def fit(self, X, Y, epochs=10, learning_rate=0.01, initialize=True, display_loss=False):
         if initialize:
             self.w = np.random.randn(1, X.shape[1])
             self.b = 0
 
-        for _ in range(epochs):
+        if display_loss:
+            loss = dict()
+
+        for i in tqdm(range(epochs), total=epochs, unit="Epochs"):
             dw = 0
             db = 0
 
@@ -34,6 +40,16 @@ class SigmoidNeuron:
 
             self.w -= learning_rate * dw
             self.b -= learning_rate * db
+
+            if display_loss:
+                Y_pred = self.sigmoid(self.perceptron(X))
+                loss[i] = mean_squared_error(Y_pred, Y)
+            
+        if display_loss:
+            plt.plot(list(loss.values()))
+            plt.xlabel('Epochs')
+            plt.ylabel('Mean Squared Error')
+            plt.show()
         
     def predit(self, X):
         Y_pred = list()
